069282ab8248f1bf6d76dcc59b98dca140044ebb
kodaimura <kodaimura123@gmail.com>
2023-01-28 14:51:54 +0900

    add products.html

--- app/controllers/customer.py
+++ app/controllers/customer.py
@@ -6,7 +6,9 @@ from flask import (
 	make_response,
 	jsonify
 )
-
+import models.general as general
+import models.product as product
+import models.category as category
 
 bp_customer = Blueprint("bp_customer", __name__, template_folder="templates")
 
@@ -14,3 +16,16 @@ bp_customer = Blueprint("bp_customer", __name__, template_folder="templates")
 @bp_customer.get("/order")
 def order_page():
 	return render_template("order.html")
+
+
+@bp_customer.get("/products")
+def products_page():
+	categories = category.get_by({})
+	products = product.get_by({"display_flg":"1"})
+	recommends = product.get_by({"display_flg":"1", "recommend_flg":"1"})
+	return render_template(
+		"products.html",
+		categories=categories,
+		products=products,
+		recommends=recommends,
+	)
