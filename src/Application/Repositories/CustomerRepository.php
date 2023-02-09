449a0756c4fe33ff08ef13141670ca3e80be20ec
kodaimura <kodaimura123@gmail.com>
2023-02-09 23:49:23 +0900

    CustomerRepository: bug fix

--- src/Application/Repositories/CustomerRepository.php
+++ src/Application/Repositories/CustomerRepository.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Application\Repositories;
 
 use \PDO;
-use Psr\Log\LoggerInterface;
 use Psr\Container\ContainerInterface;
 
 class CustomerRepository
@@ -60,8 +59,6 @@ class CustomerRepository
     	$stmt->bindValue(':customer_name', $values['customer_name'], PDO::PARAM_STR);
     	$stmt->bindValue(':cumulative_payment', $values['cumulative_payment'], PDO::PARAM_INT);
     	$stmt->execute();
-
-    	$this->app->get(LoggerInterface::class)->info(json_encode($db->errorInfo()));
     }
 
     public function update($values) {
@@ -70,12 +67,12 @@ class CustomerRepository
     		"UPDATE customer SET
     			customer_name = :customer_name,
     			visits_count = visits_count + 1,
-    			cumulative_payment = cumulative_payment + :cumulative_payment,
+    			cumulative_payment = cumulative_payment + :cumulative_payment
 		 	 WHERE customer_id = :customer_id"
     	);
-    	$stmt->bindValue(':customer_name', $values['ustomer_name'], PDO::PARAM_STR);
+    	$stmt->bindValue(':customer_name', $values['customer_name'], PDO::PARAM_STR);
     	$stmt->bindValue(':cumulative_payment', $values['cumulative_payment'], PDO::PARAM_INT);
-    	$stmt->bindValue(':customer_id', $values['$ustomer_id'], PDO::PARAM_STR);
+    	$stmt->bindValue(':customer_id', $values['customer_id'], PDO::PARAM_STR);
     	$stmt->execute();
     }
 
