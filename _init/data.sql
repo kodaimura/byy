862956739218f0d0578f323b1386ba94c05555d2
kodaimura <kodaimura123@gmail.com>
2023-02-12 11:27:34 +0900

    add ddl

---
+++ _init/data.sql
@@ -0,0 +1,12 @@
+insert into general (key1, value, remarks) values ("admin-password", "admin", "管理者用パスワード");
+insert into general (key1, value, remarks) values ("tax", "10", "消費税 率");
+
+insert into category (category_name) values ("旬のもの");
+insert into category (category_name) values ("果物");
+insert into category (category_name) values ("定番野菜");
+insert into category (category_name) values ("葉物野菜");
+insert into category (category_name) values ("根菜類");
+insert into category (category_name) values ("薬味野菜");
+insert into category (category_name) values ("菌類(きのこ類)");
+insert into category (category_name) values ("季節野菜");
+insert into category (category_name) values ("その他");
---
+++ _init/ddl.sql
@@ -0,0 +1,51 @@
+CREATE TABLE IF NOT EXISTS general (
+	key1 varchar(20) NOT NULL,
+	key2 varchar(20),
+	value varchar(30) NOT NULL,
+	remarks varchar(100),
+	create_at datetime default current_timestamp,
+	update_at datetime default current_timestamp on update current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS category (
+	category_id int PRIMARY KEY AUTO_INCREMENT,
+	category_name varchar(30) NOT NULL,
+	create_at datetime default current_timestamp,
+	update_at datetime default current_timestamp on update current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS product (
+	product_id int PRIMARY KEY AUTO_INCREMENT,
+	category_id int NOT NULL,
+	product_name varchar(30) NOT NULL,
+	production_area varchar(20),
+	unit_quantity varchar(20),
+	unit_price int NOT NULL,
+	comment varchar(300),
+	display_flg char(1) NOT NULL DEFAULT '1',
+	stock_flg char(1) NOT NULL DEFAULT '1',
+	recommend_flg char(1) NOT NULL DEFAULT '0',
+	img_name varchar(20),
+	seq int DEFAULT 0,
+	create_at datetime default current_timestamp,
+	update_at datetime default current_timestamp on update current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS order_history (
+	customer_id varchar(100),
+	product_id int,
+	order_count int NOT NULL DEFAULT 0,
+	create_at datetime default current_timestamp,
+	update_at datetime default current_timestamp on update current_timestamp,
+	PRIMARY KEY(customer_id, product_id)
+);
+
+CREATE TABLE IF NOT EXISTS customer (
+	customer_id varchar(100),
+	customer_name varchar(50),
+	visit_count int NOT NULL DEFAULT 0,
+	total_payment int NOT NULL DEFAULT 0,
+	create_at datetime default current_timestamp,
+	update_at datetime default current_timestamp on update current_timestamp,
+	PRIMARY KEY(customer_id)
+);
