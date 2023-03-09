08e53c9b782777034b4dc8ad9595199c0e80236c
kodaimura <kodaimura123@gmail.com>
2023-03-09 23:48:59 +0900

    data.sql

--- _init/data.sql
+++ _init/data.sql
@@ -3,8 +3,8 @@ insert into general (key1, value, remarks) values ("tax", "10", "消費税 率")
 
 insert into general (key1, key2, value, remarks) values ("slot-rate", "1", "0.5", "30%OFF");
 insert into general (key1, key2, value, remarks) values ("slot-rate", "2", "1", "20%OFF");
-insert into general (key1, key2, value, remarks) values ("slot-rate", "3", "1.3", "15%OFF");
-insert into general (key1, key2, value, remarks) values ("slot-rate", "4", "1.3", "10%OFF");
+insert into general (key1, key2, value, remarks) values ("slot-rate", "3", "1.3", "10%OFF");
+insert into general (key1, key2, value, remarks) values ("slot-rate", "4", "1.3", "15%OFF");
 insert into general (key1, key2, value, remarks) values ("slot-rate", "5", "2", "8%OFF");
 insert into general (key1, key2, value, remarks) values ("slot-rate", "6", "3.3", "5%OFF");
 insert into general (key1, key2, value, remarks) values ("slot-rate", "7", "5", "3%OFF");
